9f26cdbcfb45e16c039b3f051fd54efacf68d753
Андрей <rex.kudimov@mail.ru>
2022-11-03 22:43:10 +0300

    Add files via upload

---
+++ board_app/filters.py
@@ -0,0 +1,23 @@
+from django_filters import FilterSet, DateTimeFilter
+from django.forms import DateTimeInput
+from .models import Advertisement
+
+
+class AdvertisementFilter(FilterSet):
+    added_after = DateTimeFilter(
+        field_name='time_in',
+        lookup_expr='lt',
+        widget=DateTimeInput(
+            format='%Y-%m-%dT%H:%M',
+            attrs={'type': 'datetime-local'},
+        ),
+    )
+    class Meta:
+       model = Advertisement
+       fields = {
+           'heading': ['icontains'],
+           'category': ['exact'],
+           'author': ['exact']
+       }
+
+
